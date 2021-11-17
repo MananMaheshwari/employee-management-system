d3c7a305d0895d128d06e038aaaddadc6e640afc
Manan Maheshwari <maheshwarimanan0@gmail.com>
2021-11-17 14:23:37 +0530

    started the project

---
+++ src/components/Information.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Information.css'
+
+function Information() {
+    return (
+        <div className="info-container">
+            <h1 className="heading">Hello</h1>
+        </div>
+    )
+}
+
+export default Information
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './NavigationBar.css'
+
+function NavigationBar() {
+    return (
+        <div className="navbar-container">
+            <h1 className="heading">Heya</h1>
+        </div>
+        
+        
+    )
+}
+
+export default NavigationBar
