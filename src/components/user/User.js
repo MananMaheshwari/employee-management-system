fe852600b78f4fe5817b09deb6f13683d2695eb1
Hetav Patel <hetav2014@gmail.com>
2021-11-22 01:09:57 +0530

    blah

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -10,7 +10,7 @@ import './User.css';
 
 const User = (props) => {
 console.log(props);
-    return (
+    return ( 
       <div className="userMain">
         <UserNav />
         <div className="userMainClass">
